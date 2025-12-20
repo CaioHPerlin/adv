9a917a691eea214d035b28fbea850909f20431c6
caiohperlin <caiohperlin@gmail.com>
2025-12-20 11:48:09 -0400

    feat: implement prisma module

---
+++ api/src/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from './generated/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from 'src/config/environment-variables';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(configService: ConfigService<EnvironmentVariables>) {
+    const adapter = new PrismaBetterSqlite3({
+      url: configService.get('DATABASE_URL'),
+    });
+    super({ adapter });
+  }
+}
