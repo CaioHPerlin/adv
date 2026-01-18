5814e8d84e4f96d4a8b4f8c8edefabfb3646071f
caiohperlin <caiohperlin@gmail.com>
2026-01-18 18:27:59 -0400

    refactor(web): update app component and route configuration

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,18 +1,23 @@
-import { createRouter, RouterProvider } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "@tanstack/react-router";
+import { AuthProvider, useAuth } from "./contexts/auth";
+import { router } from "./router";
 
-const router = createRouter({
-	routeTree: routeTree,
-});
+function InnerApp() {
+	const auth = useAuth();
+	const queryClient = new QueryClient();
 
-declare module "@tanstack/react-router" {
-	interface RegisterRouter {
-		router: typeof router;
-	}
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} context={{ auth }} />
+		</QueryClientProvider>
+	);
 }
 
-function App() {
-	return <RouterProvider router={router} />;
+export function App() {
+	return (
+		<AuthProvider>
+			<InnerApp />
+		</AuthProvider>
+	);
 }
-
-export default App;
--- web/src/routeTree.gen.ts
+++ web/src/routeTree.gen.ts
@@ -8,51 +8,92 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from './routes/__root'
-import { Route as IndexRouteImport } from './routes/index'
+import { Route as rootRouteImport } from "./routes/__root"
+import { Route as EntrarRouteImport } from "./routes/entrar"
+import { Route as AuthRouteImport } from "./routes/_auth"
+import { Route as AuthIndexRouteImport } from "./routes/_auth.index"
 
-const IndexRoute = IndexRouteImport.update({
-  id: '/',
-  path: '/',
+const EntrarRoute = EntrarRouteImport.update({
+  id: "/entrar",
+  path: "/entrar",
   getParentRoute: () => rootRouteImport,
+} as any)
+const AuthRoute = AuthRouteImport.update({
+  id: "/_auth",
+  getParentRoute: () => rootRouteImport,
+} as any)
+const AuthIndexRoute = AuthIndexRouteImport.update({
+  id: "/",
+  path: "/",
+  getParentRoute: () => AuthRoute,
 } as any)
 
 export interface FileRoutesByFullPath {
-  '/': typeof IndexRoute
+  "/entrar": typeof EntrarRoute
+  "/": typeof AuthIndexRoute
 }
 export interface FileRoutesByTo {
-  '/': typeof IndexRoute
+  "/entrar": typeof EntrarRoute
+  "/": typeof AuthIndexRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
-  '/': typeof IndexRoute
+  "/_auth": typeof AuthRouteWithChildren
+  "/entrar": typeof EntrarRoute
+  "/_auth/": typeof AuthIndexRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: "/entrar" | "/"
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: "/entrar" | "/"
+  id: "__root__" | "/_auth" | "/entrar" | "/_auth/"
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute
+  AuthRoute: typeof AuthRouteWithChildren
+  EntrarRoute: typeof EntrarRoute
 }
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexRouteImport
+    "/entrar": {
+      id: "/entrar"
+      path: "/entrar"
+      fullPath: "/entrar"
+      preLoaderRoute: typeof EntrarRouteImport
       parentRoute: typeof rootRouteImport
+    }
+    "/_auth": {
+      id: "/_auth"
+      path: ""
+      fullPath: ""
+      preLoaderRoute: typeof AuthRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    "/_auth/": {
+      id: "/_auth/"
+      path: "/"
+      fullPath: "/"
+      preLoaderRoute: typeof AuthIndexRouteImport
+      parentRoute: typeof AuthRoute
     }
   }
 }
 
+interface AuthRouteChildren {
+  AuthIndexRoute: typeof AuthIndexRoute
+}
+
+const AuthRouteChildren: AuthRouteChildren = {
+  AuthIndexRoute: AuthIndexRoute,
+}
+
+const AuthRouteWithChildren = AuthRoute._addFileChildren(AuthRouteChildren)
+
 const rootRouteChildren: RootRouteChildren = {
-  IndexRoute: IndexRoute,
+  AuthRoute: AuthRouteWithChildren,
+  EntrarRoute: EntrarRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
