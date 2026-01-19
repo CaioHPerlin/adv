6c4a1dea94dbeea780bb98959c67d2157c47aa25
caiohperlin <caiohperlin@gmail.com>
2026-01-19 07:26:02 -0400

    feat: update create-user.dto for user registration

--- api/src/core/users/dto/create-user.dto.ts
+++ api/src/core/users/dto/create-user.dto.ts
@@ -1,7 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDto implements Partial<User> {
-  email: string;
+  @ApiProperty({ example: 'Anders Fridén' })
+  @IsString()
+  @MinLength(2)
   name: string;
+
+  @ApiProperty({ example: 'anders@inflames.com' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: '0nly4theWeak123' })
+  @IsString()
+  @MinLength(6)
   password: string;
 }
