4eed178b1ca040ee80b20b12deb9a0eacf655d13
caiohperlin <caiohperlin@gmail.com>
2026-01-19 07:26:07 -0400

    refactor: update users.module for new controller and service

--- api/src/core/users/users.module.ts
+++ api/src/core/users/users.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { HashService } from 'src/common/providers/hash.service';
-import { UsersRepository } from './users.repository';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [PrismaModule],
   providers: [UsersRepository, HashService, UsersService],
+  controllers: [UsersController],
   exports: [UsersService],
 })
 export class UsersModule {}
