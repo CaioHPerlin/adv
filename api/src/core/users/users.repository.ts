e9c49049289ff7b3880c1f3978da375ecf45aa6a
caiohperlin <caiohperlin@gmail.com>
2026-01-19 07:26:09 -0400

    feat: add findAll and findById to users.repository

--- api/src/core/users/users.repository.ts
+++ api/src/core/users/users.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from 'src/prisma/generated/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from './entities/user.entity';
-import { Prisma } from 'src/prisma/generated/client';
 
 @Injectable()
 export class UsersRepository {
@@ -10,6 +10,16 @@ export class UsersRepository {
   async findByEmail(email: string): Promise<User | null> {
     return this.prismaService.user.findUnique({
       where: { email },
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
+
+  async findById(id: number): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { id },
     });
   }
 
